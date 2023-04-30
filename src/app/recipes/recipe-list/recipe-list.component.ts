6d06ca7e106e8c169d8b2e3ec474802d4ca76834
Damian Dawidowski <damiandawidowski@gmail.com>
2023-04-30 10:59:56 +0200

    modified:   src/app/header/header.component.html 	modified:   src/app/header/header.component.ts 	modified:   src/app/recipes/recipe-list/recipe-list.component.html 	modified:   src/app/recipes/recipe-list/recipe-list.component.ts 	new file:   src/app/recipes/recipe.model.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test recipe',
+      'https://www.cookipedia.co.uk/wiki/images/4/47/Marmitako_%282%29_recipe.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a test recipe',
+      'https://www.cookipedia.co.uk/wiki/images/4/47/Marmitako_%282%29_recipe.jpg'
+    ),
+  ];
+  constructor() {}
 }
