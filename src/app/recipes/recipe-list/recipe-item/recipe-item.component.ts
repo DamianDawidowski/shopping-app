f098f0da13769646a98096557aba287d56b6b5c3
Damian Dawidowski <damiandawidowski@gmail.com>
2023-05-02 14:36:49 +0200

    modified:   src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts 	modified:   src/app/recipes/recipe-list/recipe-list.component.html

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -3,9 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent {
-@Input() recipeName: string;
- 
+  @Input() recipeName: string;
+  @Input() recipeDescription: string;
+  @Input() recipeImagePath: string;
 }
