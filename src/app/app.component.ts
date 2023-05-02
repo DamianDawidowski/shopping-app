f808e02a756ef79e0966de2da2f3e9d74caacfa5
Damian Dawidowski <damiandawidowski@gmail.com>
2023-05-02 14:28:59 +0200

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/header/header.component.html 	modified:   src/app/header/header.component.ts 	modified:   src/app/recipes/recipe-detail/recipe-detail.component.html 	modified:   src/app/recipes/recipe-list/recipe-item/recipe-item.component.html 	modified:   src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts 	modified:   src/app/recipes/recipe-list/recipe-list.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  loadedFeature: string = 'recipe'
+  onNavigate(message:string) {
+    this.loadedFeature = message;
+  }
+
   // title = 'shopping-app';
 }
